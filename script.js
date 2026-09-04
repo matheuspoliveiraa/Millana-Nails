a5ce6cf6719b2cc8d0fe09a5482c82e2c4ac4f8e
matheuspoliveiraa <matheuspoliveira353jr@gmail.com>
2026-09-03 21:08:19 -0300

    Adicionando carrossel

--- script.js
+++ script.js
@@ -22,3 +22,116 @@ const menuEsmalte = document.getElementById("menuEsmalte");
             });
 
         });
+
+        /* =========================================
+   CARROSSEL
+========================================= */
+
+const slides = document.querySelectorAll(".slide");
+const indicadores = document.querySelectorAll(".indicador");
+
+const anterior = document.getElementById("anterior");
+const proximo = document.getElementById("proximo");
+
+let slideAtual = 0;
+
+
+/* =========================================
+   MOSTRAR SLIDE
+========================================= */
+
+function mostrarSlide(numero) {
+
+    slides.forEach(function (slide) {
+
+        slide.classList.remove("ativo");
+
+    });
+
+
+    indicadores.forEach(function (indicador) {
+
+        indicador.classList.remove("ativo");
+
+    });
+
+
+    slides[numero].classList.add("ativo");
+
+    indicadores[numero].classList.add("ativo");
+
+    slideAtual = numero;
+
+}
+
+
+/* =========================================
+   PRÓXIMO
+========================================= */
+
+proximo.addEventListener("click", function () {
+
+    slideAtual++;
+
+    if (slideAtual >= slides.length) {
+
+        slideAtual = 0;
+
+    }
+
+    mostrarSlide(slideAtual);
+
+});
+
+
+/* =========================================
+   ANTERIOR
+========================================= */
+
+anterior.addEventListener("click", function () {
+
+    slideAtual--;
+
+    if (slideAtual < 0) {
+
+        slideAtual = slides.length - 1;
+
+    }
+
+    mostrarSlide(slideAtual);
+
+});
+
+
+/* =========================================
+   CLICAR NOS INDICADORES
+========================================= */
+
+indicadores.forEach(function (indicador, indice) {
+
+    indicador.addEventListener("click", function () {
+
+        mostrarSlide(indice);
+
+    });
+
+});
+
+
+/* =========================================
+   TROCA AUTOMÁTICA
+========================================= */
+
+setInterval(function () {
+
+    slideAtual++;
+
+    if (slideAtual >= slides.length) {
+
+        slideAtual = 0;
+
+    }
+
+    mostrarSlide(slideAtual);
+
+}, 5000);
